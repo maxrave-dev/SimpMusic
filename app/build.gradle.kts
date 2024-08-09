17524af8499b1744e377ef1890dad3e625b06e1f
Nguyen Duc Tuan Minh <minhnguyen.31211027595@st.ueh.edu.vn>
2024-08-09 14:14:23 +0700

    Remove shrinking resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,6 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
-            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
