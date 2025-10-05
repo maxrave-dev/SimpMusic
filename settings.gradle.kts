967169ebd3bb59a9729625d28dc3663eacf6c0f0
maxrave-dev <ndtminh2608@gmail.com>
2025-10-05 18:34:34 +0700

    feat: refactor code folder

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -37,6 +37,27 @@ val sharedDir =
         File(rootDir, "./MediaServiceCore/SharedModules")
     }
 
+val coreDir =
+    if (File(rootDir, "../core").exists()) {
+        File(rootDir, "../core")
+    } else {
+        File(rootDir, "./core")
+    }
+
+val serviceDir =
+    if (File(rootDir, "../service").exists()) {
+        File(rootDir, "../service")
+    } else {
+        File(rootDir, "./service")
+    }
+
+val mediaDir =
+    if (File(rootDir, "../media").exists()) {
+        File(rootDir, "../media")
+    } else {
+        File(rootDir, "./media")
+    }
+
 rootProject.name = "SimpMusic"
 include(
     "app",
@@ -52,11 +73,28 @@ include(
     ":googleapi",
     ":sharedtests",
     ":sharedutils",
+    ":j2v8",
+    ":media3",
+    ":media3-ui",
 )
 project(":mediaserviceinterfaces").projectDir = File(mediaServiceCore, "mediaserviceinterfaces")
 project(":youtubeapi").projectDir = File(mediaServiceCore, "youtubeapi")
 project(":googleapi").projectDir = File(mediaServiceCore, "googleapi")
 project(":sharedtests").projectDir = File(sharedDir, "sharedtests")
 project(":sharedutils").projectDir = File(sharedDir, "sharedutils")
-include(":media3")
-include(":media3-ui")
+project(":j2v8").projectDir = File(sharedDir, "j2v8")
+
+// core modules
+project(":common").projectDir = File(coreDir, "common")
+project(":data").projectDir = File(coreDir, "data")
+project(":domain").projectDir = File(coreDir, "domain")
+
+// service modules
+project(":aiService").projectDir = File(serviceDir, "aiService")
+project(":lyricsService").projectDir = File(serviceDir, "lyricsService")
+project(":kotlinYtmusicScraper").projectDir = File(serviceDir, "kotlinYtmusicScraper")
+project(":spotify").projectDir = File(serviceDir, "spotify")
+
+// media modules
+project(":media3").projectDir = File(mediaDir, "media3")
+project(":media3-ui").projectDir = File(mediaDir, "media3-ui")
