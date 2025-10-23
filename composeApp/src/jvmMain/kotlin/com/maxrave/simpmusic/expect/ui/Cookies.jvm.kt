f309077bd1f79e4e98d51974581367cac51265ab
maxrave-dev <ndtminh2608@gmail.com>
2025-10-23 16:03:49 +0700

    feat: update text

--- composeApp/src/jvmMain/kotlin/com/maxrave/simpmusic/expect/ui/Cookies.jvm.kt
+++ composeApp/src/jvmMain/kotlin/com/maxrave/simpmusic/expect/ui/Cookies.jvm.kt
@@ -5,7 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
@@ -13,6 +14,7 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import com.maxrave.simpmusic.expect.openUrl
 import com.maxrave.simpmusic.ui.theme.typo
 import java.net.CookieHandler
 import java.net.CookieManager
@@ -45,7 +47,9 @@ actual fun PlatformWebView(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Column {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Text(
                 "Because of the limitations of Compose Multiplatform, I can not implement WebView on desktop.\n" +
                     "Read this to learn more how to log in in desktop:\n",
@@ -53,6 +57,18 @@ actual fun PlatformWebView(
                 color = Color.White,
                 textAlign = TextAlign.Center
             )
+            Button(
+                onClick = {
+                    openUrl("https://www.simpmusic.org/blogs/en/how-to-log-in-on-desktop-app")
+                },
+            ) {
+                Text(
+                    "Open blog post",
+                    style = typo().labelMedium,
+                    color = Color.DarkGray,
+                    textAlign = TextAlign.Center
+                )
+            }
         }
         aboveContent()
     }
