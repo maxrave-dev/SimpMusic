133758fcd6f1d935fcaff165effde0dc1285a1d5
OpenAethel <166249695+OpenAethel@users.noreply.github.com>
2026-01-31 13:08:38 +0100

    Update lyrics_service.py

--- androidApp/src/main/python/lyrics_service.py
+++ androidApp/src/main/python/lyrics_service.py
@@ -2,9 +2,7 @@ import syncedlyrics
 
 def search_synced_lyrics(query):
     try:
-        # This uses the library you added in your build.gradle.kts
-        # It returns an LRC formatted string (with timestamps)
-        lrc_data = syncedlyrics.search(query)
+        lrc_data = syncedlyrics.search(query, enhanced = True)
         return lrc_data if lrc_data else "No lyrics found."
     except Exception as e:
         return f"Error: {str(e)}"
